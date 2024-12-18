b6581a06fa83e631577ddf0490401bdd9fac3f05
idkgene <webeugene@proton.me>
2024-12-18 17:36:41 +0300

    Awaited

---
+++ Awaited/solve.ts
@@ -0,0 +1,7 @@
+type MyAwaited<T> = T extends Promise<infer P>
+  ? P extends Promise<any>
+    ? MyAwaited<P>
+    : P
+  : T extends { then: (onfulfilled: (arg: infer U) => any) => any }
+  ? MyAwaited<U>
+  : T;
